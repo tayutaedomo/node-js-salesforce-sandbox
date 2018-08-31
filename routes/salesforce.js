41be17374089e256bfaaff15a62b856d164e4c3b
tayutaedomo <tayutaedomo@gmail.com>
2018-08-31 16:45:32 +0900

    Add salesforce/account/create

--- routes/salesforce.js
+++ routes/salesforce.js
@@ -1,8 +1,10 @@
 'use strict';
 
+var debug = require('debug')('node-js-salesforce-sandbox:routes:salesforce');
 const express = require('express');
 const router = express.Router();
 
+const Promise = require('bluebird');
 const jsforce = require('jsforce');
 
 
@@ -16,7 +18,10 @@ const OAUTH_CLIENT_PASSWORD = process.env.OAUTH_CLIENT_PASSWORD;
 
 
 router.get('/auth', function(req, res, next) {
-  res.render('salesforce/auth', { title: 'Auth', data: {} });
+  res.render('salesforce/auth', {
+    title: 'Auth',
+    data: {}
+  });
 });
 
 router.post('/auth', function(req, res, next) {
@@ -48,6 +53,76 @@ router.post('/auth', function(req, res, next) {
     res.render('salesforce/auth', payload);
   });
 });
+
+
+router.get('/account/create', function(req, res, next) {
+  res.render('salesforce/account/create', {
+    title: 'Create Account',
+    data: { params: {} }
+  });
+});
+
+router.post('/account/create', function(req, res, next) {
+  debug('/account/create body', req.body);
+
+  const params = req.body;
+  const payload =  {
+    title: 'Create Account',
+    data: { params: params }
+  };
+
+  let conn = null;
+
+  Promise.resolve().then(function() {
+    return loginAsync().then(function(connection, result) {
+      conn = connection;
+    });
+
+  }).then(function() {
+    return new Promise(function(resolve, reject) {
+      // See: https://jsforce.github.io/document/#create
+      const p = {
+        Name: params.name
+      };
+
+      conn.sobject("Account").create(p, function(err, result) {
+        if (err || !result.success) {
+          console.error(result);
+          reject(err);
+
+        } else {
+          payload.data.result = JSON.stringify(result, null, 2);
+          resolve();
+        }
+      });
+    });
+
+  }).catch(function(err) {
+    console.error(err);
+    payload.data.error = err;
+
+  }).finally(function() {
+    res.render('salesforce/account/create', payload);
+  });
+});
+
+
+function loginAsync() {
+  const conn = new jsforce.Connection({
+    oauth2: {
+      loginUrl:     OAUTH_LOGIN_URL,
+      clientId:     OAUTH_CONSUMER_KEY,
+      clientSecret: OAUTH_CONSUMER_SECRET
+    }
+  });
+
+  return new Promise(function(resolve, reject) {
+    conn.login(OAUTH_CLIENT_USERNAME, OAUTH_CLIENT_PASSWORD, function(err, result) {
+      if (err) reject(err);
+      else resolve(conn, result);
+    });
+  });
+}
 
 
 module.exports = router;
