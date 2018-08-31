c4ebc08a55c54d159917562241835e42bfaf89b7
tayutaedomo <tayutaedomo@gmail.com>
2018-08-31 19:38:45 +0900

    Add ipify

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,36 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
+
+var request = require('request');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Index' });
 });
+
+
+router.get('/ipify', function(req, res, next) {
+  const payload =  {
+    title: 'ipify',
+    data: {}
+  };
+
+  request('http://api.ipify.org', function (error, response, body) {
+    if (error) {
+      console.error('error:', error);
+      payload.data.error = error;
+
+    } else {
+      payload.data.result = body
+    }
+
+    res.render('ipify/get', payload);
+  });
+});
+
 
 module.exports = router;
 
