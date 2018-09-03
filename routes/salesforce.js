7c4a7154f6963e3575ce25bab217208b03f5df70
tayutaedomo <tayutaedomo@gmail.com>
2018-09-03 18:52:56 +0900

    Add salesforce/contact/create

--- routes/salesforce.js
+++ routes/salesforce.js
@@ -112,16 +112,56 @@ router.post('/contact/query', function(req, res, next) {
   });
 });
 
-// router.get('/contact/create', function(req, res, next) {
-//   res.render('salesforce/contact/create', {
-//     title: 'Create Account',
-//     data: { params: {} }
-//   });
-// });
-//
-// router.post('/contact/create', function(req, res, next) {
-//   debug('/contact/create body', req.body);
-// });
+router.get('/contact/create', function(req, res, next) {
+  res.render('salesforce/contact/create', {
+    title: 'Create Contact',
+    data: { params: {} }
+  });
+});
+
+router.post('/contact/create', function(req, res, next) {
+  debug('/contact/create body', req.body);
+
+  const params = req.body;
+  const payload =  {
+    title: 'Create Contact',
+    data: { params: params }
+  };
+
+  let conn = null;
+
+  Promise.resolve().then(function() {
+    return loginAsync().then(function(result) {
+      conn = result.conn;
+    });
+
+  }).then(function() {
+    return new Promise(function(resolve, reject) {
+      const p = {
+        LastName: params.lastName,
+        Email: params.email
+      };
+
+      conn.sobject('Contact').create(p, function(err, result) {
+        if (err || !result.success) {
+          console.error(result);
+          reject(err);
+
+        } else {
+          payload.data.result = JSON.stringify(result, null, 2);
+          resolve();
+        }
+      });
+    });
+
+  }).catch(function(err) {
+    console.error(err);
+    payload.data.error = err;
+
+  }).finally(function() {
+    res.render('salesforce/contact/create', payload);
+  });
+});
 
 
 router.get('/account/query', function(req, res, next) {
