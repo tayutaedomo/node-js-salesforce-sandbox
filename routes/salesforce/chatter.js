565a077ed69f257672af89880e5e70d1f5cd8a0d
tayutaedomo <tayutaedomo@gmail.com>
2018-09-18 14:16:50 +0900

    Add salesforce/chatter/group/post

--- routes/salesforce/chatter.js
+++ routes/salesforce/chatter.js
@@ -50,5 +50,65 @@ router.get('/me', function(req, res, next) {
 });
 
 
+router.get('/group/post', function(req, res, next) {
+  res.render('salesforce/chatter/group/post', {
+    title: 'Chatter Post to Group',
+    data: { params: {} }
+  });
+});
+
+router.post('/group/post', function(req, res, next) {
+  const params = req.body;
+  const payload =  {
+    title: 'Chatter Post to Group',
+    data: { params: params }
+  };
+
+  let conn = null;
+
+  Promise.resolve().then(function() {
+    return salesforce.loginAsync().then(function(result) {
+      debug('loginAsync result', result.result);
+      conn = result.conn;
+    });
+
+  }).then(function() {
+    return new Promise(function(resolve, reject) {
+      //
+      // See: https://jsforce.github.io/document/#post-a-feed-item
+      //
+      const p = {
+        body: {
+          messageSegments: [{
+            type: 'Text',
+            text: params.message
+          }]
+        },
+        feedElementType : 'FeedItem',
+        subjectId: params.groupId
+      };
+
+      conn.chatter.resource('/feed-elements').create(p, function(err, result) {
+        if (err) {
+          console.error(result);
+          reject(err);
+
+        } else {
+          payload.data.result = JSON.stringify(result, null, 2);
+          resolve();
+        }
+      });
+    });
+
+  }).catch(function(err) {
+    console.error(err);
+    payload.data.error = err;
+
+  }).finally(function() {
+    res.render('salesforce/chatter/group/post', payload);
+  });
+});
+
+
 module.exports = router;
 
